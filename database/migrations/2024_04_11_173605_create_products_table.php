e7fe53d4706f7a9d5af861be332e85f413f272c5
MahdiIzadi <MahdiIzadi@mahdidida7@gmail.com>
2024-04-21 15:07:45 +0330

    Your commit message here

--- database/migrations/2024_04_11_173605_create_products_table.php
+++ database/migrations/2024_04_11_173605_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image');
             $table->string('desc');
             $table->string('sku');
             $table->foreignId('category_id')->constrained('product_categories');
